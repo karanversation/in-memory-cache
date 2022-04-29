73d2962795544eee945aef5590c40844d7f15d9a
karanversation <karanbajaj23@gmail.com>
2022-04-29 14:49:17 +0530

    dll.py

--- cache.py
+++ cache.py
@@ -1,5 +1,7 @@
 # Author: Karan Bajaj (karanbajaj23@gmail.com)
 
+from dll import DoublyLinkedList
+
 
 class NotImplementedException(Exception):
     pass
@@ -57,53 +59,6 @@ class Node:
         self._next = _next # reference to _next node in DLL
         self.prev = prev # reference to previous node in DLL
         self.data = data
-
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
-
-    def push(self, new_data):
-        new_node = Node(new_data)
-        new_node._next = self.head
-        if self.head is not None:
-            self.head.prev = new_node
-        self.head = new_node
-
-    def insertAfter(self, prev_node, new_data):
-        if prev_node is None:
-            print("the given previous node cannot be NULL")
-            return
-        new_node = Node(new_data)
-        new_node._next = prev_node._next
-        prev_node._next = new_node
-        new_node.prev = prev_node
-        if new_node._next:
-            new_node._next.prev = new_node
-
-    def append(self, new_data):
-        new_node = Node(new_data)
-        if self.head is None:
-            self.head = new_node
-            return
-        last = self.head
-        while last._next:
-            last = last._next
-        last._next = new_node
-        new_node.prev = last
-        return
-
-    def printList(self, node):
-        print("\nTraversal in forward direction")
-        while node:
-            print(" {}".format(node.data))
-            last = node
-            node = node._next
-        print("\nTraversal in reverse direction")
-        while last:
-            print(" {}".format(last.data))
-            last = last.prev
-
 
 cache = Cache('lru', 100)
 
---
+++ dll.py
@@ -0,0 +1,48 @@
+# Author: Karan Bajaj (karanbajaj23@gmail.com)
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node._next = self.head
+        if self.head is not None:
+            self.head.prev = new_node
+        self.head = new_node
+
+    def insertAfter(self, prev_node, new_data):
+        if prev_node is None:
+            print("previous node cannot be NULL")
+            return
+        new_node = Node(new_data)
+        new_node._next = prev_node._next
+        prev_node._next = new_node
+        new_node.prev = prev_node
+        if new_node._next:
+            new_node._next.prev = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last._next:
+            last = last._next
+        last._next = new_node
+        new_node.prev = last
+        return
+
+    def printList(self, node):
+        print("\nTraversal in forward direction")
+        while node:
+            print(" {}".format(node.data))
+            last = node
+            node = node._next
+        print("\nTraversal in reverse direction")
+        while last:
+            print(" {}".format(last.data))
+            last = last.prev
+
