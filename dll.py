3761bf125f15c958397022dd42d24a7c4f6034aa
karanversation <karanbajaj23@gmail.com>
2022-04-30 18:50:27 +0530

    sanitize method names

--- dll.py
+++ dll.py
@@ -15,14 +15,14 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-    def push(self, new_data):
+    def insert_head(self, new_data):
         new_node = Node(data=new_data)
         new_node._next = self.head
         if self.head is not None:
             self.head.prev = new_node
         self.head = new_node
 
-    def insertAfter(self, prev_node, new_data):
+    def insert_after(self, prev_node, new_data):
         if prev_node is None:
             print("previous node cannot be NULL")
             return
@@ -33,7 +33,7 @@ class DoublyLinkedList:
         if new_node._next:
             new_node._next.prev = new_node
 
-    def append(self, new_data):
+    def insert_tail(self, new_data):
         new_node = Node(data=new_data)
         if self.head is None:
             self.head = new_node
@@ -45,7 +45,7 @@ class DoublyLinkedList:
         new_node.prev = last
         return
 
-    def printList(self, node=None):
+    def print(self, node=None):
         node = node or self.head
         print("Traversal in forward direction")
         while node:
